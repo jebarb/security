c0493019bd1c757603682763dc23b215173413e3
James Barbour <jebarbou@cs.unc.edu>
2017-02-11 11:47:36 -0500

    removed duplicate files

--- homework1/c_asm_familiarization/low_level_info.c
+++ homework1/c_asm_familiarization/low_level_info.c
@@ -13,16 +13,12 @@ int GetCurrentDirectoryContents(char **names, int max_files) {
   struct dirent *ent;
   int num_files;
 
-  // can use "." instead of getcwd()
   // get and open cwd, return errno on fail
   if (getcwd(cwd, sizeof(cwd)) == NULL || (dir = opendir(cwd)) == NULL)
     return errno;
 
   // for each file in cwd, malloc and copy name to names
-  for (num_files = 0; (ent = readdir(dir)) != NULL && num_files < max_files; 
-      ++num_files) {
-    // can use strndup instead of malloc, memcpy
-    // should be strlen, not sizeof
+  for (num_files = 0; (ent = readdir(dir)) != NULL && num_files < max_files; ++num_files) {
     if ((names[num_files] = malloc(sizeof(ent->d_name))) == NULL)
       return errno;
     memcpy(names[num_files], ent->d_name, sizeof(ent->d_name));
