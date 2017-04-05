65aa09e323187cd9e15c0a5423e279ec0d6d444b
James Barbour <jebarbou@cs.unc.edu>
2017-04-05 12:50:40 -0400

    added decrypt function

--- homework5/encrypt.c
+++ homework5/encrypt.c
@@ -13,15 +13,16 @@ void handleErrors(void)
   abort();
 }
 
-int encrypt(unsigned char *plaintext, int plaintext_len, unsigned long seed, unsigned char *ciphertext)
-{
+int encrypt(unsigned char *plaintext, int plaintext_len, unsigned long unix_time,
+    unsigned int pid, unsigned int nanosecond, unsigned char *ciphertext) {
   EVP_CIPHER_CTX *ctx;
 
   int len, ciphertext_len;
   int i;
 
   // set random seed
-  srand(seed);
+  unsigned int utime = unix_time & 0xffffffff;
+  srand(utime ^ pid ^ nanosecond);
 
   // create key and iv
   unsigned char key[32], iv[16];
@@ -50,6 +51,42 @@ int encrypt(unsigned char *plaintext, int plaintext_len, unsigned long seed, uns
   return ciphertext_len;
 }
 
+int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned long seed, unsigned char *plaintext) {
+  EVP_CIPHER_CTX *ctx;
+
+  int len, plaintext_len;
+  int i;
+
+  // set random seed
+  srand(seed);
+
+  // create key and iv
+  unsigned char key[32], iv[16];
+  for (i = 0; i < 32; i++)
+    key[i] = rand() & 0xff;
+  for (i = 0; i < 16; i++)
+    iv[i] = rand() & 0xff;
+
+  /* Create and initialise the context */
+  if(!(ctx = EVP_CIPHER_CTX_new())) handleErrors();
+
+  // AES 256, CTR mode
+  if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_ctr(), NULL, key, iv))
+    handleErrors();
+
+  if(1 != EVP_DecryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
+    handleErrors();
+  plaintext_len = len;
+
+  if(1 != EVP_DecryptFinal_ex(ctx, ciphertext + len, &len)) handleErrors();
+  plaintext_len += len;
+
+  /* Clean up */
+  EVP_CIPHER_CTX_free(ctx);
+
+  return plaintext_len;
+}
+
 int check_if_ascii(unsigned char *plaintext, int plaintext_len) {
   while (plaintext_len-- > 0)
     if (isascii((int) *plaintext++)) return 1;
@@ -69,14 +106,14 @@ int main(int argc, char *argv[]) {
     for (int lo = 0; lo + utime_lo < max_num && utime_lo - lo >= 0; ++lo) {
       if (utime_hi + hi < max_num) {
         if (utime_lo + lo < max_num) {
-          plaintext_len = encrypt(ciphertext, ciphertext_len, utime + hi + lo, plaintext);
+          plaintext_len = decrypt(ciphertext, ciphertext_len, utime + hi + lo, plaintext);
           if (!check_if_ascii(plaintext, plaintext_len)) {
             fwrite(plaintext, plaintext_len, 1, stdout);
             return 0;
           }
         }
         if (utime_lo - lo > 0) {
-          plaintext_len = encrypt(ciphertext, ciphertext_len, utime + hi - lo, plaintext);
+          plaintext_len = decrypt(ciphertext, ciphertext_len, utime + hi - lo, plaintext);
           if (!check_if_ascii(plaintext, plaintext_len)) {
             fwrite(plaintext, plaintext_len, 1, stdout);
             return 0;
@@ -85,14 +122,14 @@ int main(int argc, char *argv[]) {
       }
       if (hi > 0 && utime_hi - hi > 0) {
         if (utime_lo + lo < max_num) {
-          plaintext_len = encrypt(ciphertext, ciphertext_len, utime - hi + lo, plaintext);
+          plaintext_len = decrypt(ciphertext, ciphertext_len, utime - hi + lo, plaintext);
           if (!check_if_ascii(plaintext, plaintext_len)) {
             fwrite(plaintext, plaintext_len, 1, stdout);
             return 0;
           }
         }
         if (utime_lo - lo > 0) {
-          plaintext_len = encrypt(ciphertext, ciphertext_len, utime - hi - lo, plaintext);
+          plaintext_len = decrypt(ciphertext, ciphertext_len, utime - hi - lo, plaintext);
           if (!check_if_ascii(plaintext, plaintext_len)) {
             fwrite(plaintext, plaintext_len, 1, stdout);
             return 0;
