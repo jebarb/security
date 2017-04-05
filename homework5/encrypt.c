0e6e03b5be1be7f2fe3860f0554c2d4f10da560d
James Barbour <jebarbou@cs.unc.edu>
2017-04-05 11:29:40 -0400

    hw5 basic progress

--- homework5/encrypt.c
+++ homework5/encrypt.c
@@ -1,6 +1,7 @@
 #include <openssl/conf.h>
 #include <openssl/evp.h>
 #include <openssl/err.h>
+#include <ctype.h>
 
 /**********
  * NOTE: This file cannot be compiled in its current state.
@@ -12,8 +13,7 @@ void handleErrors(void)
   abort();
 }
 
-int encrypt(unsigned char *plaintext, int plaintext_len, unsigned long unix_time,
-    unsigned int pid, unsigned int nanosecond, unsigned char *ciphertext)
+int encrypt(unsigned char *plaintext, int plaintext_len, unsigned long seed, unsigned char *ciphertext)
 {
   EVP_CIPHER_CTX *ctx;
 
@@ -21,8 +21,7 @@ int encrypt(unsigned char *plaintext, int plaintext_len, unsigned long unix_time
   int i;
 
   // set random seed
-  unsigned int utime = unix_time & 0xffffffff;
-  srand(utime ^ pid ^ nanosecond);
+  srand(seed);
 
   // create key and iv
   unsigned char key[32], iv[16];
@@ -49,4 +48,43 @@ int encrypt(unsigned char *plaintext, int plaintext_len, unsigned long unix_time
   EVP_CIPHER_CTX_free(ctx);
 
   return ciphertext_len;
+}
+
+int check_if_ascii(unsigned char *plaintext, int plaintext_len) {
+  while (plaintext_len-- > 0)
+    if (isascii((int) *plaintext++)) return 1;
+  return 0;
+}
+
+int main(int argc, char *argv[]) {
+  int max_num = 65536;
+  int utime_hi = 0;
+  int utime_lo = 0;
+  int utime = 0;
+  unsigned char *plaintext;
+  int plaintext_len;
+  unsigned char *ciphertext;
+  int ciphertext_len = 0;
+  for (int hi = 0; hi + utime_hi < max_num && utime_hi - hi >= 0; ++hi) {
+    for (int lo = 0; lo + utime_lo < max_num && utime_lo - lo >= 0; ++lo) {
+      if (utime_hi + hi < max_num) {
+        if (utime_lo + lo < max_num) {
+          plaintext_len = encrypt(ciphertext, ciphertext_len, utime + hi + lo, plaintext);
+          if (!check_if_ascii(plaintext, plaintext_len))  {
+
+              }
+
+        if (utime_lo - lo > 0)
+          plaintext_len = encrypt(ciphertext, ciphertext_len, utime + hi - lo, plaintext);
+      }
+      if (hi > 0 && utime_hi - hi > 0) {
+        if (utime_lo + lo < max_num)
+          plaintext_len = encrypt(ciphertext, ciphertext_len, utime - hi + lo, plaintext);
+        if (utime_lo - lo > 0)
+          plaintext_len = encrypt(ciphertext, ciphertext_len, utime - hi - lo, plaintext);
+      }
+    }
+  }
+
+  return 0;
 }
