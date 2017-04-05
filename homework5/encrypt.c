0e34e18cfbb3c033215751d1a53562939048747f
James Barbour <jebarbou@cs.unc.edu>
2017-04-05 14:23:53 -0400

    encrypt working

--- homework5/encrypt.c
+++ homework5/encrypt.c
@@ -15,7 +15,7 @@ void handleErrors(void)
 }
 
 int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
-  unsigned char *iv, unsigned char *plaintext)
+    unsigned char *iv, unsigned char *plaintext)
 {
   EVP_CIPHER_CTX *ctx;
 
@@ -64,42 +64,6 @@ void get_key_iv(unsigned long seed, unsigned char *key, unsigned char *iv){
     iv[i] = rand() & 0xff;
 }
 
-int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned long seed, unsigned char *plaintext) {
-  EVP_CIPHER_CTX *ctx;
-
-  int len, plaintext_len;
-  int i;
-
-  // set random seed
-  srand(seed);
-
-  // create key and iv
-  unsigned char key[32], iv[16];
-  for (i = 0; i < 32; i++)
-    key[i] = rand() & 0xff;
-  for (i = 0; i < 16; i++)
-    iv[i] = rand() & 0xff;
-
-  /* Create and initialise the context */
-  if(!(ctx = EVP_CIPHER_CTX_new())) handleErrors();
-
-  // AES 256, CTR mode
-  if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_ctr(), NULL, key, iv))
-    handleErrors();
-
-  if(1 != EVP_DecryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
-    handleErrors();
-  plaintext_len = len;
-
-  if(1 != EVP_DecryptFinal_ex(ctx, ciphertext + len, &len)) handleErrors();
-  plaintext_len += len;
-
-  /* Clean up */
-  EVP_CIPHER_CTX_free(ctx);
-
-  return plaintext_len;
-}
-
 int check_if_ascii(unsigned char *plaintext, int plaintext_len) {
   while (plaintext_len-- > 0)
     if (!isascii((int) *plaintext++)) return 0;
@@ -108,16 +72,16 @@ int check_if_ascii(unsigned char *plaintext, int plaintext_len) {
 
 unsigned int force_decrypt(int utime_start, unsigned char *ciphertext, int ciphertext_len, unsigned char *plaintext){
   /*
-    Starting at the given timestamp, 
-    iterate in both directions MODIFYING LOW ORDER BITS FIRST
-  */
+     Starting at the given timestamp, 
+     iterate in both directions MODIFYING LOW ORDER BITS FIRST
+     */
   unsigned int utime_upper = utime_start & 0xffff0000; // Mask off lower 16
   unsigned int utime_inc_value = 0x00010000; // 2^16
   unsigned int utime_plus = utime_upper;
   unsigned int utime_minus = utime_upper - utime_inc_value;
   unsigned int plaintext_len;
   unsigned char key[32], iv[16];
-  
+
   while (1){
     // iterate over lower bits
     for (unsigned int lower_bits = 0; lower_bits <= 0xffff; lower_bits++){
@@ -126,11 +90,11 @@ unsigned int force_decrypt(int utime_start, unsigned char *ciphertext, int ciphe
         get_key_iv(seed, key, iv);
         plaintext_len = decrypt(ciphertext, ciphertext_len, key, iv, plaintext);
         if (check_if_ascii(plaintext, plaintext_len))
-          return seed;        
+          return seed;
       }
       if (utime_minus){
         unsigned int seed = (unsigned long) (utime_plus | lower_bits);
-        get_key_iv(seed, &key, &iv);
+        get_key_iv(seed, key, iv);
         plaintext_len = decrypt(ciphertext, ciphertext_len, key, iv, plaintext);
         if (check_if_ascii(plaintext, plaintext_len))
           return seed;
@@ -161,7 +125,6 @@ int main(int argc, char *argv[]) {
   unsigned char * input_name = argv[1];
   unsigned char * output_name = argv[2];
   FILE *ifile = fopen(input_name, "r");
-  FILE *ofile = fopen(output_name, "w");
 
   // INPUT
   fseek(ifile, 0, SEEK_END); 
@@ -175,11 +138,13 @@ int main(int argc, char *argv[]) {
   // OUTPUT
   unsigned char* unencrypted = malloc(sizeof(unsigned char)*size);
   int seed = force_decrypt(time(NULL), ciphertext, (int)size, unencrypted);
-  
+
   if (seed){
+    // puts(unencrypted);
+    FILE *ofile = fopen(output_name, "w");
     fprintf(ofile, "%s", unencrypted);
     fclose(ofile);
-    printf("%x", seed);
+    printf("%x\n", seed);
   }
   return 0;
 }
